f4ff385ade0912d577e42e9d09c3d3083b4a6258
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2022-09-09 00:23:47 +0600

    update firefox extension

--- background-ff.js
+++ background-ff.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (tab.url.match(/^https:\/\/github.com\/.*/) || tab.url.match(/^https:\/\/gitlab.com\/.*/)) {
+  if (tab.url.match(/^https:\/\/github.com\/.*/) || tab.url.match(/^https:\/\/gitlab.com\/.*/)|| tab.url.match(/^https:\/\/bitbucket.org\/.*/)) {
     browser.browserAction.enable(tabId);
   } else {
     browser.browserAction.disable(tabId);
@@ -7,7 +7,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 });
 
 chrome.tabs.onCreated.addListener(function(tab) {
-  if (tab.url && (tab.url.match(/^https:\/\/github.com\/.*/)|| tab.url.match(/^https:\/\/gitlab.com\/.*/))) {
+  if (tab.url && (tab.url.match(/^https:\/\/github.com\/.*/)|| tab.url.match(/^https:\/\/gitlab.com\/.*/)|| tab.url.match(/^https:\/\/bitbucket.org\/.*/))) {
     browser.browserAction.enable(tabId);
   } else if (tab.url) {
     browser.browserAction.disable(tabId);
@@ -23,11 +23,19 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 browser.browserAction.onClicked.addListener((tab) => {
+  chrome.storage.sync.get({
+    protocol: 'HTTPS'
+  }, function(items) {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-        const url = new URL(tabs[0].url);
-        const path = url.pathname.split('/');
-        if (path[1] !== undefined && path[2] !== undefined) {
+      const url = new URL(tabs[0].url);
+      const path = url.pathname.split('/');
+      if (path[1] !== undefined && path[2] !== undefined) {
+        if(items.protocol=='HTTPS'){
           chrome.tabs.create({url: "vscode://vscode.git/clone?url=https://" + url.hostname + "/" + path[1] + "/" + path[2] + ".git"});
+        } else if (items.protocol=='SSH'){
+          chrome.tabs.create({url: "vscode://vscode.git/clone?url=git@" + url.hostname + ":" + path[1] + "/" + path[2] + ".git"});
         }
+      }
     });
+  });
   })
