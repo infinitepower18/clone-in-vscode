875af12f520436bd27debb4e6e35e634638c7f4a
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2022-09-08 20:19:44 +0600

    ssh support

--- background.js
+++ background.js
@@ -29,11 +29,19 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 
 chrome.action.onClicked.addListener((tab) => {
+  chrome.storage.sync.get({
+    protocol: 'HTTPS'
+  }, function(items) {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-        const url = new URL(tabs[0].url);
-        const path = url.pathname.split('/');
-        if (path[1] !== undefined && path[2] !== undefined) {
+      const url = new URL(tabs[0].url);
+      const path = url.pathname.split('/');
+      if (path[1] !== undefined && path[2] !== undefined) {
+        if(items.protocol=='HTTPS'){
           chrome.tabs.create({url: "vscode://vscode.git/clone?url=https://" + url.hostname + "/" + path[1] + "/" + path[2] + ".git"});
+        } else if (items.protocol=='SSH'){
+          chrome.tabs.create({url: "vscode://vscode.git/clone?url=git@" + url.hostname + ":" + path[1] + "/" + path[2] + ".git"});
         }
+      }
     });
+  });
   })
