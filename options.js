a82cb6e5daf546ce6ab37999b64315da570d0dfd
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2022-09-08 19:47:15 +0600

    add ssh option

---
+++ options.js
@@ -0,0 +1,20 @@
+function save_options() {
+    var protocol = document.getElementById('protocol').value;
+    chrome.storage.sync.set({
+      protocol: protocol
+    }, function() {
+      var status = document.getElementById('status');
+      status.textContent = 'Options saved.';
+    });
+  }
+  
+  function restore_options() {
+    chrome.storage.sync.get({
+      protocol: 'HTTPS'
+    }, function(items) {
+      document.getElementById('protocol').value = items.protocol;
+    });
+  }
+  document.addEventListener('DOMContentLoaded', restore_options);
+  document.getElementById('save').addEventListener('click',
+      save_options);
