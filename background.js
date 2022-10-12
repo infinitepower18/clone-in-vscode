75a798eb728aa986db3cea43acf92d81b2105c83
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2022-10-12 14:35:40 +0100

    add insiders and vscodium support

--- background.js
+++ background.js
@@ -34,16 +34,24 @@ chrome.action.onClicked.addListener((tab) => {
   chrome.storage.sync.get(
     {
       protocol: "HTTPS",
+      application: "VS Code",
     },
     function (items) {
       chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
         const url = new URL(tabs[0].url);
         const path = url.pathname.split("/");
+        if(items.application == "VS Code"){
+          openApp = "vscode";
+        } else if (items.application == "VS Code Insiders"){
+          openApp = "vscode-insiders";
+        } else if (items.application == "VSCodium"){
+          openApp = "vscodium";
+        }
         if (path[1] !== undefined && path[2] !== undefined) {
           if (items.protocol == "HTTPS") {
             chrome.tabs.create({
               url:
-                "vscode://vscode.git/clone?url=https://" +
+                openApp + "://vscode.git/clone?url=https://" +
                 url.hostname +
                 "/" +
                 path[1] +
@@ -54,7 +62,7 @@ chrome.action.onClicked.addListener((tab) => {
           } else if (items.protocol == "SSH") {
             chrome.tabs.create({
               url:
-                "vscode://vscode.git/clone?url=git@" +
+                openApp + "://vscode.git/clone?url=git@" +
                 url.hostname +
                 ":" +
                 path[1] +
--- options.js
+++ options.js
@@ -1,12 +1,14 @@
 function save_options() {
   var protocol = document.getElementById("protocol").value;
+  var application = document.getElementById("application").value;
   chrome.storage.sync.set(
     {
       protocol: protocol,
+      application: application,
     },
     function () {
       var status = document.getElementById("status");
-      status.textContent = "Option saved.";
+      status.textContent = "Options saved.";
       setTimeout(function () {
         status.textContent = "";
       }, 3000);
@@ -18,9 +20,11 @@ function restore_options() {
   chrome.storage.sync.get(
     {
       protocol: "HTTPS",
+      application: "VS Code",
     },
     function (items) {
       document.getElementById("protocol").value = items.protocol;
+      document.getElementById("application").value = items.application;
     }
   );
 }
